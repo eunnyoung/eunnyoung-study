b16050ccf00ad21c0b986675b6199f6d4cc79bbd
SEO EUNYOUNG <eyoung6u6@daum.net>
2022-03-13 16:00:59 +0900

    vector 공부

--- c++/220311/220311/main.cpp
+++ c++/220311/220311/main.cpp
@@ -1,22 +1,135 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	int row = 5;
+	int row = 3;
+	int col = 5;
+	// 크기가 자동으로 늘어나고 줄어드는 배열
+	vector< vector<int> > va;
+
+	for (int i = 0; i < row; ++i) {
+		va.push_back(vector<int>());
+		for (int j = 0; j < col; ++j)
+			va[i].push_back(i * 100 + j);
+	}
+
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < col; ++j)
+			printf("%5d", va[i][j]);
+		printf("\n");
+	}
+}
+
+/*
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int row = 3;
+	int col = 5;
+	// 크기가 자동으로 늘어나고 줄어드는 배열
+	vector< vector<int> > va;
+
+	for (int i = 0; i < row; ++i) {
+		va.push_back(vector<int>());
+		for (int j = 0; j < col; ++j)
+			va[i].push_back(i * 100 + j);
+	}
+
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < col; ++j)
+			printf("%5d", va[i][j]);
+		printf("\n");
+	}
+}
+*/
+
+/*
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	// 크기가 자동으로 늘어나고 줄어드는 배열
+	vector< vector<int> > va;
+
+	for (int i = 0; i < 5; ++i)
+		//벡터 맨 뒤에 값을 넣는다.
+		va.push_back( vector<int>() );
+
+	//va[0] ~ va[2]
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 5; ++j)
+			va[i].push_back(i * 100 + j);
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 5; ++j)
+			printf("%5d", va[i][j]);
+		printf("\n");
+	}
+}
+*/
+
+/*
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	// 크기가 자동으로 늘어나고 줄어드는 배열
+	vector<int> arr;
+
+	for (int i = 0; i < 5; ++i)
+		//벡터 맨 뒤에 값을 넣는다.
+		arr.push_back(i + 1);
+
+	for (int i = 0; i < 5; ++i)
+		printf("%5d", arr[i]);
+}
+*/
+
+/*
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int** CreateArray(int row, int col)
+{
+	int** pa = new int* [row];
+	for (int i = 0; i < row; ++i)
+		pa[i] = new int[col];
+
+	return pa;
+}
+void FreeArray(int** pa, int row, int col)
+{
+	for (int i = 0; i < row; ++i)
+		delete[] pa[i];
+
+	delete[] pa;
+}
+int main()
+{
+	int row = 10;
 	int col = 5;
 	int** pa = NULL;
 
 	printf(" 행 입력:");
-	scanf_s("%d", row);
+	scanf_s("%d", &row);
 	printf(" 열 입력:");
 	scanf_s("%d", &col);
 
-	pa = new int* [row];
-
-	for (int i = 0; i < 5; ++i)
-		pa[i] = new int[col];
+	pa = CreateArray(row, col);
 
 	for (int i = 0; i < row; ++i)
 		for (int j = 0; j < col; ++j)
@@ -29,12 +142,9 @@ int main()
 		printf("\n");
 	}
 
-	for (int i = 0; i < row; ++i)
-		delete[] pa[i];
-
-	delete[] pa;
+	FreeArray(pa, row, col);
 }
-
+*/
 
 /*
 #include <iostream>
