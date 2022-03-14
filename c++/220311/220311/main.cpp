b7e9151cf1120a81ef9d68b54b362cce039b4f9b
SEO EUNYOUNG <eyoung6u6@daum.net>
2022-03-14 17:50:25 +0900

    자기참조 구조체 공부

--- c++/220311/220311/main.cpp
+++ c++/220311/220311/main.cpp
@@ -1,3 +1,167 @@
+//#include <iostream>
+#include <stdio.h>
+//using namespace std;
+struct Node
+{
+	int data;
+	Node* link;
+}; // 노드 정의(자기참조구조체)
+
+Node* AllocNode(int data)
+{
+	Node* n = new Node;
+	n->data = 10;
+	n->link = NULL;
+
+	return n;
+}
+int main()
+{
+	Node* h = NULL;
+
+	h = AllocNode(10);
+
+	h->link = AllocNode(20);
+
+	h->link->link = AllocNode(30);
+
+	Node* p = h;
+	printf("%d\n", p->data);
+	p = p->link;
+	printf("%d\n", p->data);
+	p = p->link;
+	printf("%d\n", p->data);
+	p = p->link;
+	printf("%d\n", p->data);
+	p = p->link;
+	printf("%d\n", p->data);
+
+	for (Node* p = h; p != NULL; p = p->link)
+		printf("%d\n", p->data);
+}
+
+////#include <iostream>
+//#include <stdio.h>
+////using namespace std;
+//struct Node
+//{
+//	int data;
+//	Node* link;
+//}; // 노드 정의(자기참조구조체)
+//int main()
+//{
+//	Node* h = NULL;
+//
+//	h = new Node;
+//	h->data = 10;
+//	h->link = NULL;
+//
+//	h->link = new Node;
+//	h->link->data = 20;
+//	h->link->link = NULL;
+//
+//	h->link->link = new Node;
+//	h->link->link->data = 30;
+//	h->link->link->link = NULL;
+//
+//	h->link->link->link = new Node;
+//	h->link->link->link->data = 40;
+//	h->link->link->link->link = NULL;
+//
+//	h->link->link->link->link = new Node;
+//	h->link->link->link->link->data = 50;
+//	h->link->link->link->link->link = NULL;
+//
+//	Node* p = h;
+//	printf("%d\n", p->data);
+//	p = p->link;
+//	printf("%d\n", p->data);
+//	p = p->link;
+//	printf("%d\n", p->data);
+//	p = p->link;
+//	printf("%d\n", p->data);
+//	p = p->link;
+//	printf("%d\n", p->data);
+//
+//	for (Node* p = h; p != NULL; p = p->link)
+//		printf("%d\n", p->data);
+//}
+
+////#include <iostream>
+//#include <stdio.h>
+////using namespace std;
+//struct Node
+//{
+//	int data;
+//	Node* link;
+//}; // 노드 정의(자기참조구조체)
+//int main()
+//{
+//	Node* h = NULL;
+//
+//	h = new Node;
+//	h->data = 10;
+//	h->link = NULL;
+//
+//	h->link = new Node;
+//	h->link->data = 30;
+//	h->link->link = NULL;
+//
+//	h->link->link = new Node;
+//	h->link->link->data = 30;
+//	h->link->link->link = NULL;
+//
+//	h->link->link->link = new Node;
+//	h->link->link->link->data = 40;
+//	h->link->link->link->link = NULL;
+//
+//	h->link->link->link->link = new Node;
+//	h->link->link->link->link->data = 50;
+//	h->link->link->link->link->link = NULL;
+//
+//	printf("%d %d %d\n", h->data, h->link->data, h->link->link->data,
+//		h->link->link->link->data, h->link->link->link->link->data);
+//}
+
+////#include <iostream>
+//#include <stdio.h>
+////using namespace std;
+//struct Node
+//{
+//	int data;
+//	Node* link;
+//}; // 노드 정의(자기참조구조체)
+//int main()
+//{
+//	Node n1 = { 10, NULL };
+//	Node n2 = { 20, NULL };
+//	Node n3 = { 30, NULL };
+//	Node* h = &n1;
+//	// n1.link = &n2;
+//	// 구조체 포인터 안의 변수에 접근하게 해준다.
+//	h->link = &n2;
+//
+//	// n2.link = &n3;
+//	h->link->link = &n3;
+//
+//	printf("%d %d %d\n", n1.data, n2.data, n3.data);
+//	printf("%d %d %d\n", h->data, h->link->data, h->link->link->data);
+//}
+
+////#include <iostream>
+//#include <stdio.h>
+////using namespace std;
+//struct Node
+//{
+//	int data;
+//	Node* link;
+//}; // 노드 정의(자기참조구조체)
+//int main()
+//{
+//	Node n1 = {10, NULL};
+//}
+
+/*
 #include <iostream>
 #include <stdio.h>
 #include <vector>
@@ -22,6 +186,7 @@ int main()
 		printf("\n");
 	}
 }
+*/
 
 /*
 #include <iostream>
