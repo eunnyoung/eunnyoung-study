fa29fa74b1e8fd8fb045dba52d5573208f4557c2
eunnyoung <12dreamer30@gmail.com>
2022-10-16 19:17:53 +0900

    Feat: 221016 그림판 만들기 연습

---
+++ HTML_CSS_ JS/그림판 만들기/1010/app.js
@@ -0,0 +1,6 @@
+// let 변수
+let isDrawing = false;
+let isFilling = false;
+// color 변수
+const colors = Array.from(document.getElementsByClassName("color"));
+const color = document.getElementsByClassName("color");
