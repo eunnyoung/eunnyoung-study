04c0d6131c3aad14f21eeaba287030169bccd39c
eunnyoung <12dreamer30@gmail.com>
2022-11-09 14:29:37 +0900

    Feat: 1109 js 연습

---
+++ HTML_CSS_ JS/그림판 만들기/1109_2/app.js
@@ -0,0 +1,115 @@
+let isDrawing = false;
+let isFilling = false;
+const colors = Array.from(document.getElementsByClassName("color"));
+const colorRange = document.getElementById("colorRange");
+const lineWidth = document.getElementById("lineWidth");
+const file = document.getElementById("file");
+const text = document.getElementById("text");
+const btnMode = document.getElementById("btnMode");
+const btnDestroy = document.getElementById("btnDestroy");
+const btnErase = document.getElementById("btnErase");
+const btnSave = document.getElementById("btnSave");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineCap = "round";
+ctx.lineWidth = lineWidth.lineWidth;
+// functions
+function onMove(event) {
+  if (isDrawing) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startDrawing() {
+  isDrawing = true;
+}
+function cancelDrawing() {
+  isDrawing = false;
+  ctx.beginPath();
+}
+function clickCanvas() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+function clickColor(event) {
+  const clickedColor = event.target.dataset.color;
+  ctx.strokeStyle = clickedColor;
+  ctx.fillStyle = clickedColor;
+  colorRange.value = clickedColor;
+}
+function clickDouble(event) {
+  const text = text.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "68px sans-serif";
+    ctx.fillRect(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+function clickMode() {
+  if (isFilling) {
+    isFilling = false;
+    btnMode.innerText = "Fill";
+  } else {
+    isFilling = true;
+    btnMode.innerText = "Draw";
+  }
+}
+function clickDestroy() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+function clickErase() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  btnMode.innerText = "Fill";
+}
+function clickSave() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+function changeFile(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    file.value = null;
+  };
+}
+function changeColor(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+function changeLineWidth(event) {
+  ctx.lineWidth = event.target.value;
+}
+// eventListenter
+canvas.addEventListener("click", clickCanvas);
+canvas.addEventListener("dblclick", clickDouble);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mouseleave", cancelDrawing);
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mouseup", cancelDrawing);
+lineWidth.addEventListener("change", changeLineWidth);
+file.addEventListener("change", changeFile);
+colorRange.addEventListener("change", changeColor);
+colors.forEach((colorRange) =>
+  colorRange.addEventListener("click", clickColor)
+);
+btnMode.addEventListener("click", clickMode);
+btnDestroy.addEventListener("click", clickDestroy);
+btnErase.addEventListener("click", clickErase);
+btnSave.addEventListener("click", clickSave);
--- HTML_CSS_ JS/크롬 웹 만들기/Feedback.md
+++ HTML_CSS_ JS/크롬 웹 만들기/Feedback.md
@@ -1,16 +1,10 @@
-### 1030
-
-- 문제: todo list 기능이 잘 되지 않는다.
-  <br/>preventDefault 가 제대로 타이핑이 안 된 건가 싶어서 todo.js 파일을 다시 훑어봤다.
-  <pre>
-    // const newTodo = toDoInput.ariaValueMax;
-  const newTodo = toDoInput.value;
-  </pre>
-  위와 같이 코드를 수정해도 여전히 투두리스트 input 박스 enter 시 페이지 새로고침 되는 문제가 발생하고 있다.
-
-### 공부 내용
+## 공부 내용
 
 - padStart
 - innerText
 - 템플릿 리터럴
 - localStorage
+
+### parseInt
+
+- parse: (문장을 문법적으로 분석하다)
